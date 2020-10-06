534409a0fa637de3936c7364020d40fbb8850a6c
Myckhel <myckhel1@hotmail.com>
2020-10-06 21:22:54 +0100

    business db seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,12 +23,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $faker->name,
-            'email' => $faker->unique()->safeEmail,
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'lat' => $faker->latitude,
-            'lng' => $faker->longitude,
+            'lat' => $this->faker->latitude,
+            'lng' => $this->faker->longitude,
             'activation_token' => Str::random(10),
             'remember_token' => Str::random(10),
         ];
@@ -56,6 +56,6 @@ class UserFactory extends Factory
         } catch (\Exception $e) {
           print($e->getMessage());
         }
-      }
+      });
     }
 }
