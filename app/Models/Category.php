f9afbf917556e96bf6b5626fa4772268de59020b
myckhel <myckhel1@hotmail.com>
2022-02-13 02:26:43 +0100

    saved dev

---
+++ app/Models/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasMeta;
+use App\Traits\Searchable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory, Searchable, HasMeta;
+    protected $fillable = [];
+    protected $casts    = [];
+    protected $searches = [];
+}
