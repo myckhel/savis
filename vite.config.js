39eaed24695b1ac627e6e55d352f65ce81a66b45
myckhel <myckhel1@hotmail.com>
2022-04-15 14:09:43 +0100

    remove auto jsx

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import laravel from "vite-plugin-laravel";
-import reactJsx from "vite-react-jsx";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
@@ -7,7 +6,6 @@ import svgrPlugin from "vite-plugin-svgr";
 export default defineConfig({
   plugins: [
     react(),
-    reactJsx(),
     laravel(),
     svgrPlugin({
       svgrOptions: {
