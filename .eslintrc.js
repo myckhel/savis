275140c50f71f9362bbf1d000e9f8a53d71dd7a4
Michael Ishola <myckhel1@hotmail.com>
2022-07-03 03:47:48 +0100

    Vite bump (#22)
    * upgraded to laravel v9
    
    * vite upgraded
    
    * bumped inertia
    
    * eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,49 @@
 module.exports = {
   root: true,
   extends: [
-    "standard",
-    "standard-react",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jest/recommended",
-    "plugin:jest/style",
-    "plugin:react/jsx-runtime",
+    'standard',
+    'standard-react',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:react/jsx-runtime'
   ],
   env: {
     browser: true,
     node: true,
     es6: true,
-    "jest/globals": true,
+    'jest/globals': true
   },
   globals: {
     route: true,
     Echo: true,
-    Pusher: true,
+    Pusher: true
   },
-  plugins: ["react", "jest"],
+  plugins: ['react', 'jest'],
   parserOptions: {
     ecmaVersion: 2020,
     ecmaFeatures: {
       legacyDecorators: true,
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   settings: {
     react: {
-      version: "17",
+      version: '17'
     },
     jest: {
-      version: "latest",
-    },
+      version: 'latest'
+    }
   },
   rules: {
-    "no-shadow": 0,
-    "no-eval": 0,
+    'no-shadow': 0,
+    'no-eval': 0,
     camelcase: 0,
-    "react-hooks/exhaustive-deps": 0,
-    "import/no-cycle": "error",
-    "react/jsx-uses-react": 0,
-    "react/react-in-jsx-scope": 0,
-    "jest/no-commented-out-tests": 0,
-  },
-};
+    'react-hooks/exhaustive-deps': 0,
+    'import/no-cycle': 'error',
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'jest/no-commented-out-tests': 0
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import laravel from "vite-plugin-laravel";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgrPlugin from "vite-plugin-svgr";
+import laravel from 'vite-plugin-laravel'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgrPlugin from 'vite-plugin-svgr'
 
 export default defineConfig({
   plugins: [
@@ -9,8 +9,8 @@ export default defineConfig({
     laravel(),
     svgrPlugin({
       svgrOptions: {
-        icon: true,
-      },
-    }),
-  ],
-});
+        icon: true
+      }
+    })
+  ]
+})
