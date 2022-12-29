0bf06b644b798c157016f60145a10742ecb8a62f
Myckhel <myckhel1@hotmail.com>
2022-12-29 20:53:33 +0100

    bumped react

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,40 +1,31 @@
-import 'nouislider/distribute/nouislider.css'
-import '../css/app.css'
-import '../css/custom.css'
-import '../css/style.css'
-import './bootstrap'
+import 'nouislider/distribute/nouislider.css';
+import '../css/app.css';
+import '../css/custom.css';
+import '../css/style.css';
+import './bootstrap';
 
-import { render } from 'react-dom'
-import { createInertiaApp } from '@inertiajs/inertia-react'
-import { InertiaProgress } from '@inertiajs/progress'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import SimpleReactLightbox from 'simple-react-lightbox'
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/inertia-react';
+import { InertiaProgress } from '@inertiajs/progress';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import SimpleReactLightbox from 'simple-react-lightbox';
 
-InertiaProgress.init()
+InertiaProgress.init();
 createInertiaApp({
-  resolve: async (name) => {
-    const pages = import.meta.glob('./pages/**/*.jsx')
-    console.log({ pages })
+  resolve: async name => {
+    const pages = import.meta.glob('./Pages/**/*.jsx');
+    const page = Object.keys(pages).find(page => page.endsWith(`${name}.jsx`));
 
-    for (const path in pages) {
-      if (path.endsWith(`${name.replace('.', '/')}.jsx`)) {
-        return typeof pages[path] === 'function'
-          ? (await pages[path]()).default
-          : pages[path]
-      }
-    }
-
-    throw new Error(`Page not found: ${name}`)
+    return (await pages[page]()).default;
   },
-  setup ({ el, App, props }) {
-    render(
+  setup({ el, App, props }) {
+    createRoot(el).render(
       <Provider store={store}>
         <SimpleReactLightbox>
           <App {...props} />
         </SimpleReactLightbox>
-      </Provider>,
-      el
-    )
+      </Provider>
+    );
   }
-})
+});
