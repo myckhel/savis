ec10b8be8a7b40cb01d617447224c2fb71239c30
Myckhel <myckhel1@hotmail.com>
2022-12-30 01:06:55 +0100

    added antd

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,7 @@
 import '../css/app.css';
 import '../sass/app.scss';
 import './bootstrap';
+import 'antd/dist/reset.css';
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/inertia-react';
@@ -8,6 +9,7 @@ import { InertiaProgress } from '@inertiajs/progress';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ConfigProvider } from 'antd';
 
 InertiaProgress.init();
 createInertiaApp({
@@ -19,7 +21,9 @@ createInertiaApp({
   setup({ el, App, props }) {
     createRoot(el).render(
       <Provider store={store}>
-        <App {...props} />
+        <ConfigProvider>
+          <App {...props} />
+        </ConfigProvider>
       </Provider>
     );
   }
