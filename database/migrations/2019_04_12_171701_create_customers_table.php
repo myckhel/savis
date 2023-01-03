ce10cfe73280ceb5cd54980e1a15564eb747b1f7
Myckhel <myckhel1@hotmail.com>
2023-01-03 03:04:09 +0100

    67bcd2036244ef43f7d2fd4484290fa10bf7f713 customer

--- database/migrations/2019_04_12_171701_create_customers_table.php
+++ database/migrations/2019_04_12_171701_create_customers_table.php
@@ -6,31 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCustomersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-      Schema::create('customers', function (Blueprint $table) {
-        $table->id();
-        $table->bigInteger('business_id')->unsigned();
-        $table->bigInteger('user_id')->unsigned();
-        $table->json('metas')->default('{}');
-        $table->timestamps();
-        $table->foreign('business_id')->references('id')->on('businesses')->onDelete('cascade');
-        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-      });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('customers', function (Blueprint $table) {
+      $table->id();
+      $table->bigInteger('business_id')->unsigned();
+      $table->bigInteger('user_id')->unsigned();
+      $table->json('metas'); //->default('{}');
+      $table->timestamps();
+      $table->foreign('business_id')->references('id')->on('businesses')->onDelete('cascade');
+      $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('customers');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('customers');
+  }
 }
