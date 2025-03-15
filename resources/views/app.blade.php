7983e3b9dc89c91e9e90c3fb4eddc56a8efec54d
myckhel <myckhel1@hotmail.com>
2025-03-15 16:33:32 +0100

    ts fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" />
   <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚙️</text></svg>">
   @viteReactRefresh
-  @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+  @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
   @routes
   @inertiaHead
 </head>
